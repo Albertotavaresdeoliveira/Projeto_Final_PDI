fdf52b2c7d8f98b529897a5712ce5fe8566bc7ac
Albertotavaresdeoliveira <berto.tavares@hotmail.com>
2021-09-10 09:05:12 -0300

    alterações

--- ProjetoFinal.cpp
+++ ProjetoFinal.cpp
@@ -4,68 +4,158 @@
 using namespace cv;
 using namespace std;
 
-int main(int argc, char** argv){
-  Mat image, escalacinza, histAnterior, histNovo;
-  int width, height;
-  VideoCapture cap;
+Point Findimage(Mat scene, Mat img){
+  Point p(150, 150);
+  Mat scenetmp;
+
+  Mat histscene[3];//Histogramas RGB da cena de busca
+  Mat histimg[3];//Histogramas RGB da imagem buscada
+  Mat sceneplanes[3];
+  Mat imgplanes[3];
+  
+  double Dif[3];
+  double DifSoma;
+  double MenorDif = 0;//Valor inicial alto
+
+  int scenewidth = scene.cols;
+  int sceneheight = scene.rows;
+  int imgwidth = img.cols;
+  int imgheight = img.rows;
+  int radius = (sqrt( imgwidth*imgheight + imgheight*imgheight )) / 2;
+  
+  cout <<"Dimenções da cena : (" << scenewidth << "," << sceneheight << ") " << endl;
+  cout <<"Dimenções da imagem : (" << imgwidth << "," << imgheight << ") " << endl;
+
   int nbins = 64;
   float range[] = {0, 255};
   const float *histrange = { range };
   bool uniform = true;
   bool acummulate = false;
+
+  int Ndivs = 3;
+
+  int Numwidth = (scenewidth/(imgwidth/Ndivs));// * Ndivs;
+  int Numheight = (sceneheight/(imgheight/Ndivs));// * Ndivs;
+  
+  Rect R;
+
+  for(int i=0; i<Numheight-Ndivs+1; i++){
+    for(int j=0; j<Numwidth-Ndivs+1; j++){
+
+      //scenetmp = scene(Rect(x, y, imgwidth, imgheight));//Pedaço da cena com o tamanho da imagem buscada
+      scenetmp = scene(Rect(j*(imgwidth/Ndivs), i*(imgheight/Ndivs), imgwidth, imgheight));
+      split (scenetmp, sceneplanes);
+      split (img, imgplanes);
+
+      DifSoma = 0;
+      
+      for(int z=0; z<3; z++) { 
+        calcHist(&sceneplanes[z], 1, 0, Mat(), histscene[z], 1, &nbins, &histrange, uniform, acummulate);
+        calcHist(&imgplanes[z], 1, 0, Mat(), histimg[z], 1, &nbins, &histrange, uniform, acummulate); 
+      }
+
+      //for(int d=0; d<3; d++) { Dif[d] = compareHist(histscene[d], histimg[d], HISTCMP_CORREL); }
+      for(int d=0; d<3; d++) { Dif[d] = compareHist(histscene[d], histimg[d], HISTCMP_INTERSECT); }
+  
+      for(int s=0; s<3; s++) { DifSoma = DifSoma + Dif[s]; }
+
+      if(DifSoma >= MenorDif) { MenorDif = DifSoma; p.y = (i+1)*(imgheight/Ndivs); p.x = (j+1)*(imgwidth/Ndivs);}
+      
+    }
+  }
+
+  circle(scene, p, radius, Scalar(0, 0, 255), 5, LINE_8 );
+
+  imshow("cena de busca", scene);
+  imshow("imagem buscada", img);
+
+  return p;
+}
+
+Point M, N; // Pontos para selecionar a imagem buscada na cena
+Mat scene;
+bool Clicked = false;
+
+void locator(int event, int x, int y, int flags, void* userdata){ //function to track mouse movement and click//
+   if (event == EVENT_LBUTTONDOWN){ //when left button clicked//
+      cout << "Left click has been made, Position:(" << x << "," << y << ")" << endl;
+      M.x = x; M.y = y;
+      circle(scene, M, 5, Scalar(0, 0, 255), 3, LINE_8 );
+      Clicked = true;
+      imshow("cena", scene);
+   } else if (event == EVENT_RBUTTONDOWN){ //when right button clicked//
+      cout << "Rightclick has been made, Position:(" << x << "," << y << ")" << endl;
+   } else if (event == EVENT_MBUTTONDOWN){ //when middle button clicked//
+      cout << "Middleclick has been made, Position:(" << x << "," << y << ")" << endl;
+   } else if (event == EVENT_MOUSEMOVE){ //when mouse pointer moves//
+      cout << "Current mouse position:(" << x << "," << y << ")" << endl;
+      N.x = x; N.y = y;
+      //if(Clicked) { rectangle(scene, M, N, Scalar(0, 0, 255), 2, LINE_8, 0); }
+   } else if (event == EVENT_LBUTTONUP){ //when left button is released
+      cout << "Left click up has been made, Position:(" << x << "," << y << ")" << endl;
+      rectangle(scene, M, N, Scalar(0, 0, 255), 2, LINE_8, 0);
+      circle(scene, M, 5, Scalar(0, 0, 255), 3, LINE_8 );
+      imshow("cena", scene);
+   }
+}
+
+int main(int argc, char** argv){
+  Mat img;
+  Point P;
+  VideoCapture cap;
   int key;
 
-  cap.open(0);
-  if(!cap.isOpened()){ cout << "cameras indisponiveis"; return -1; }
+  namedWindow("cena");
+  setMouseCallback("cena", locator, NULL);
 
-  cap.set(CAP_PROP_FRAME_WIDTH, 640);
-  cap.set(CAP_PROP_FRAME_HEIGHT, 480);
-  width = cap.get(CAP_PROP_FRAME_WIDTH);
-  height = cap.get(CAP_PROP_FRAME_HEIGHT);
+	cap.open(0);
 
-  Point Centro(width/2, height/2);
+  if(!cap.isOpened()){ cout << "camera indisponivel"; return -1; }
 
-  cout << "largura = " << width << endl;
-  cout << "altura  = " << height << endl;
-
-  int histw = nbins, histh = nbins/2;
-  Mat histImgAnterior(histh, histw, CV_8UC1, Scalar(0));
-  Mat histImgNovo(histh, histw, CV_8UC1, Scalar(0));
-
-  double Dif = 0, Limiar = 0.999;
+  cap.set(cv::CAP_PROP_FRAME_WIDTH, 640);//640
+  cap.set(cv::CAP_PROP_FRAME_HEIGHT, 480);//480
   
-  cap >> image;
-  cvtColor(image,escalacinza, COLOR_RGB2GRAY);
-  calcHist(&escalacinza, 1, 0, Mat(), histAnterior, 1, &nbins, &histrange, uniform, acummulate);
-  normalize(histAnterior, histAnterior, 0, histImgAnterior.rows, NORM_MINMAX, -1, Mat());
+  cout << "contrast :" << cap.get(CAP_PROP_CONTRAST) << endl;
+  cout << "brilho :" << cap.get(CAP_PROP_BRIGHTNESS) << endl;
+  cout << "saturation :" << cap.get(CAP_PROP_SATURATION) << endl;
+  
+  //img = imread(argv[1], IMREAD_COLOR);
 
   while(1){
-    cap >> image;
-    cvtColor(image,escalacinza, COLOR_RGB2GRAY);
-    calcHist(&escalacinza, 1, 0, Mat(), histNovo, 1, &nbins, &histrange, uniform, acummulate);
-    normalize(histNovo, histNovo, 0, histImgNovo.rows, NORM_MINMAX, -1, Mat());
+    cap >> scene;
 
-    histImgNovo.setTo(Scalar(0));
+    //scene = imread(argv[1], IMREAD_COLOR);
+    //img = imread(argv[2], IMREAD_COLOR);
 
-    Dif = compareHist(histAnterior, histNovo, HISTCMP_CORREL);
-    //Dif = compareHist(histAnterior, histNovo, HISTCMP_CHISQR);
-    //Dif = compareHist(histAnterior, histNovo, HISTCMP_INTERSECT);
-    //Dif = compareHist(histAnterior, histNovo, HISTCMP_BHATTACHARYYA);
-    //Dif = compareHist(histAnterior, histNovo, HISTCMP_HELLINGER );
-
-    if(Dif<=Limiar) { circle(escalacinza, Centro, 50, Scalar(255, 255, 255), FILLED, LINE_8 ); }
-
-    for(int i=0; i<nbins; i++){
-      line(histImgNovo, Point(i, histh), Point(i, histh-cvRound(histNovo.at<float>(i))), Scalar(255, 255, 255), 1, 8, 0);
-    }
-
-    histImgNovo.copyTo(escalacinza(Rect(0, 0, nbins, histh)));
-    
-    histAnterior = histNovo.clone();
-
-    imshow("image", escalacinza);
+    //P = Findimage(scene, img);
+    imshow("cena", scene);
     key = waitKey(30);
     if(key == 27) break;
   }
+
+  waitKey();
+
+  scene(Rect(M, N)).copyTo(img);
+  imshow("img", img);
+
+  waitKey();
+
+  while(1){
+    cap >> scene;
+
+    //scene = imread(argv[1], IMREAD_COLOR);
+    //img = imread(argv[2], IMREAD_COLOR);
+
+    P = Findimage(scene, img);
+    imshow("cena", scene);
+    key = waitKey(30);
+    if(key == 27) break;
+  }
+
+  cout <<"Ponto (" << P.x << "," << P.y << ") é o centro" << endl;
+
+  destroyAllWindows();
+  //waitKey();
+
   return 0;
 }
